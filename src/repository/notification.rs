4b4373a226522690f2b09af891f45d60d0de113d
Rekkin99 <farrellbagoes04@gmail.com>
2026-03-30 22:13:34 +0700

    Implement add function in Notification repository.

--- src/repository/notification.rs
+++ src/repository/notification.rs
@@ -1,6 +1,6 @@
 use std::sync::RwLock;
 use lazy_static::lazy_static;
-use crate::model::notification::Notification;
+use crate::model::notification::{self, Notification};
 
 // Singleton of Database
 lazy_static!{
@@ -10,5 +10,9 @@ lazy_static!{
 pub struct NotificationRepository;
 
 impl NotificationRepository{
-    
+    pub fn add(notification: Notification) -> Notification{
+        NOTIFICATIONS.write().unwrap()
+            .push(notification.clone());
+        return notification;
+    }
 }
